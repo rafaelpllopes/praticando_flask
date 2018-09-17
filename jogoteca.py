c6f18577c6911f91391a57e49bbd04d5c5867601
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-17 20:03:24 -0300

    Trabalhando com method post e criando novas rotas para cadastrar itens na lista.

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,  request
 
 app = Flask(__name__)
 
@@ -8,9 +8,27 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    lista = [Jogo('Super Mario', 'Ação', 'SNES'), Jogo('Pokemon Gold', 'RPG', 'GBA'), Jogo('Mortal Kombat', 'Luta', 'SNES')]
+
+lista = []
+lista.append(Jogo('Super Mario', 'Ação', 'SNES'))
+lista.append(Jogo('Pokemon Gold', 'RPG', 'GBA'))
+lista.append(Jogo('Mortal Kombat', 'Luta', 'SNES'))
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run(port=8000)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    lista.append(Jogo(nome, categoria, console))
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run(port=8000, debug=True)
